4772771a7fa0dc59d1f3373af17a1c9f761074de
Kajetan <kajetankarp@gmail.com>
2023-06-22 16:48:03 +0200

    Reconstructing main
    update of main ; preparation for new engine

--- tcp_webcam_receiver/main.java
+++ tcp_webcam_receiver/main.java
@@ -1,7 +1,7 @@
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
+import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.Socket;
@@ -9,66 +9,63 @@ import java.util.Base64;
 
 public class main {
     public static String seed = "80t43er[g;l'gp[45wies-0--sdfg";
-    public static String ip = "127.0.0.1";
+    public static String ip = "46.41.141.14";
     public static int port = 42060;
     static private Socket socket;
-    static private PrintWriter out;
     static private BufferedReader in;
     public static String packet="";
 
-
-    public static BufferedImage buffer_image;
-    public static String string_image;
     public static BufferedImage out_buff;
     public static InputStream out_stream;
-    public static void main(String[] args) throws IOException, InterruptedException {
 
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    new frame();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            PEngine engine = new PEngine();
+            new Thread(engine).start();
         });
 
-        while (true){
-            connect(ip,port);
+        while (connect(ip,port)){
             receivePacket();
+        }
+
+    }
+
+
+
+    public static boolean connect(String ip, int port){
+        try{
+            socket = new Socket(ip,port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            //System.out.println("[Receiver] Connection estabilished");
+            return true;
+        }catch (IOException e){
+            System.out.print("[Receiver] Connection failed");
+            return false;
         }
     }
 
-    public static void connect(String ip,int port){
-        try{
-            socket = new Socket(ip,port);
-            out = new PrintWriter(socket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        }catch (IOException e){
-            System.out.print("not sent");
-        }
+    public static String decrypt(String packet){
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword(seed);
+        return encryptor.decrypt(packet);
     }
 
     public static void receivePacket()  {
 
 try{
-    packet=in.readLine();
-    StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-    encryptor.setPassword(seed);
-    packet = encryptor.decrypt(packet);
+    packet=in.readLine(); ; packet = decrypt(packet);
 
     byte[] image_byte = Base64.getDecoder().decode(packet);
 
     out_stream = new ByteArrayInputStream(image_byte);
     out_buff = ImageIO.read(out_stream);
     
-    out.close();
-    in.close();
-}catch (IOException e){
-    System.out.println("a");
+}catch (NullPointerException | IOException e){
+    System.out.println(" & No input found");
 }
 
     }
 
-}
+    }
+
